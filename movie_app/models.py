1f44fc53bbf5a49af8bbc6e8973d08483d4cd1cc
Anton <golubovskiy.anton@yandex.ru>
2024-06-26 06:48:39 +0300

    Django course 4.12

---
+++ movie_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=40)
+    rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=1000000)
+
+    def __str__(self):
+        return f'{self.name} - {self.rating}%'
