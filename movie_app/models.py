9b25294d9f1a30910736bbac2ba90fceddfb8889
Anton <golubovskiy.anton@yandex.ru>
2024-07-02 07:05:39 +0300

    Django course 4.12

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -8,9 +9,19 @@ class Movie(models.Model):
     rating = models.IntegerField()
     year = models.IntegerField(null=True)
     budget = models.IntegerField(default=1000000)
+    slug = models.SlugField(default='', null=False, db_index=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Movie, self).save(*args,**kwargs)
 
     def get_url(self):
-        return reverse('movie-detail', args=[self.id])
+        return reverse('movie-detail', args=[self.slug])
 
     def __str__(self):
         return f'{self.name} - {self.rating}%'
+
+
+
+
+# from movie_app.models import Movie
--- movie_app/views.py
+++ movie_app/views.py
@@ -10,8 +10,8 @@ def show_all_movie(request):
         'movies':movies
     })
 
-def show_one_movie(request, id_movie: int):
-    movie = get_object_or_404(Movie, id=id_movie)
+def show_one_movie(request, slug_movie: str):
+    movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
         'movie':movie
     })
