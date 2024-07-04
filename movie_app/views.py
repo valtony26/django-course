a083ce7a11637f08c0f7bd3f703feca0d2837368
Anton <golubovskiy.anton@yandex.ru>
2024-07-04 06:51:00 +0300

    Django course 4.13

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 
 # Create your views here.
 from .models import Movie
@@ -6,12 +7,21 @@ from .models import Movie
 
 def show_all_movie(request):
     movies = Movie.objects.order_by('year')
+    movies = Movie.objects.annotate(
+        true_bool=Value(True),
+        false_bool=Value(False),
+        str_field=Value('hello'),
+        new_budget=F('budget')+100
+    )
+    agg = movies.aggregate(Avg('budget'), Min('rating'), Max('rating'), Count('id'))
     return render(request, 'movie_app/all_movies.html', {
-        'movies':movies
+        'movies': movies,
+        'agg': agg
     })
+
 
 def show_one_movie(request, slug_movie: str):
     movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
-        'movie':movie
+        'movie': movie,
     })
