d59d5b313a2db29bbdad4e9eab7c18b46cffba53
Anton <golubovskiy.anton@yandex.ru>
2024-07-11 06:27:49 +0300

    Django course 5.9

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,15 +1,49 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Movie
+from django.db.models import QuerySet
+from django.utils.text import slugify
+from django.core.validators import MaxValueValidator
 
 
 # Register your models here.
 
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', 'Низкий'),
+            ('от 40 до 59', 'Средний'),
+            ('от 60 до 79', 'Высокий'),
+            ('>=80', 'Наивысший')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value()=='<40':
+            return queryset.filter(rating__lt=40)
+        if self.value()=='от 40 до 59':
+            return queryset.filter(rating__gte=40).filter(rating__lt=60)
+        if self.value()=='от 60 до 79':
+            return queryset.filter(rating__gte=60).filter(rating__lt=80)
+        if self.value()=='>=80':
+            return queryset.filter(rating__gte=80)
+        return queryset
+
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
-    list_editable = ['rating', 'year', 'budget']
+    # fields = ['name', 'rating']
+    # exclude = ['slug', 'budget']
+    # readonly_fields = ['budget']
+    list_display = ['name', 'rating', 'currency', 'budget', 'rating_status']
+    list_editable = ['rating', 'currency', 'budget']
     ordering = ['-rating', 'name']
     list_per_page = 10
+    actions = ['set_dollars', 'set_euro']
+    search_fields = ['name__startswith', 'rating']
+    list_filter = ['name', 'currency', RatingFilter]
 
     @admin.display(ordering='rating', description='Статус')
     def rating_status(self, mov: Movie):
@@ -20,3 +54,17 @@ class MovieAdmin(admin.ModelAdmin):
         if mov.rating <= 85:
             return 'Зачёт'
         return 'Топчик'
+
+    @admin.action(description='Установить валюту в доллар')
+    def set_dollars(self, request, qs: QuerySet):
+        qs.update(currency=Movie.USD)
+
+
+    @admin.action(description='Установить валюту в Евро')
+    def set_euro(self, request, qs: QuerySet):
+        count_updated = qs.update(currency=Movie.EUR)
+        self.message_user(
+            request,
+            f'Было обновлено {count_updated} записей',
+            messages.ERROR
+        )
